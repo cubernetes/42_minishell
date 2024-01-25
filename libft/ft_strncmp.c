fd7e4e1d41d26cce2458b0d1ecf4a12b0e7bd9d4
tosu <timo42@proton.me>
2024-01-25 23:07:22 +0100

    Rename parameter len' in ft_strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,23 +6,23 @@
 /*   By: tischmid <tischmid@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 15:07:15 by tischmid          #+#    #+#             */
-/*   Updated: 2023/11/22 15:07:15 by tischmid         ###   ########.fr       */
+/*   Updated: 2024/01/25 20:10:27 by tosuman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdio.h>
 
-int	ft_strncmp(char const *s1, char const *s2, size_t n)
+int	ft_strncmp(char const *s1, char const *s2, size_t len)
 {
 	unsigned char	*us1;
 	unsigned char	*us2;
 
-	if (!n)
+	if (!len)
 		return (0);
 	us1 = (unsigned char *) s1;
 	us2 = (unsigned char *) s2;
-	while (*us1 && *us2 && *us1 == *us2 && --n)
+	while (*us1 && *us2 && *us1 == *us2 && --len)
 	{
 		us1++;
 		us2++;
