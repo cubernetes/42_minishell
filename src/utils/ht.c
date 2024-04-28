8bd74b6c2ba528f3cdf6ef420b3d839d7faef1ad
cubernetes <tischmid@proton.me>
2024-04-29 00:22:57 +0200

    remove specialized ht_set and ht_get functions

--- src/utils/ht.c
+++ src/utils/ht.c
@@ -6,11 +6,12 @@
 /*   By: tosuman <timo42@proton.me>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 12:49:11 by tosuman           #+#    #+#             */
-/*   Updated: 2024/04/24 23:20:27 by tischmid         ###   ########.fr       */
+/*   Updated: 2024/04/28 22:51:01 by tischmid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+#include "minishell_debug.h"
 #include <stdlib.h>
 
 static uint64_t	fnv_1a_64(char *input_str)
@@ -34,42 +35,20 @@ static uint64_t	hash(char *key)
 	return (fnv_1a_64(key));
 }
 
-void	ht_set(t_kv ht[TABLE_SIZE], char key[static 1], t_type value)
+void	ht_set(t_kv *ht[TABLE_SIZE], char key[static 1], t_type value)
 {
 	int		idx;
 	t_kv	*new_kv;
 
 	idx = (int)(hash(key) % TABLE_SIZE);
-	if (!ht[idx].k)
-	{
-		ht[idx].k = key;
-		ht[idx].v = value;
-		ht[idx].n = NULL;
-	}
-	else
-	{
-		new_kv = ft_malloc(sizeof(*new_kv));
-		new_kv->k = key;
-		new_kv->v = value;
-		new_kv->n = ht[idx].n;
-		ht[idx].n = new_kv;
-	}
-}
-
-void	ht_set_malloc(t_kv *ht[TABLE_SIZE], char key[static 1], t_type value)
-{
-	int		idx;
-	t_kv	*new_kv;
-
-	idx = (int)(hash(key) % TABLE_SIZE);
-	new_kv = malloc(sizeof(*new_kv));
+	new_kv = ft_malloc(sizeof(*new_kv));
 	new_kv->k = key;
 	new_kv->v = value;
 	new_kv->n = ht[idx];
 	ht[idx] = new_kv;
 }
 
-t_type	ht_get_malloc(t_kv *ht[TABLE_SIZE], char key[static 1])
+t_type	ht_get(t_kv *ht[TABLE_SIZE], char key[static 1])
 {
 	t_kv	*kv;
 
@@ -81,19 +60,7 @@ t_type	ht_get_malloc(t_kv *ht[TABLE_SIZE], char key[static 1])
 	return ((t_type){0});
 }
 
-t_type	ht_get(t_kv ht[TABLE_SIZE], char key[static 1])
-{
-	t_kv	*kv;
-
-	kv = &ht[hash(key) % TABLE_SIZE];
-	while (kv && kv->k && ft_strcmp(kv->k, key))
-		kv = kv->n;
-	if (kv && kv->k)
-		return (kv->v);
-	return ((t_type){0});
-}
-
-/* print must not print any newlines */
+/* print() must not print any newlines */
 void	ht_print(t_kv ht[TABLE_SIZE], void (print)(char *k, void *v))
 {
 	int		i;
@@ -125,22 +92,18 @@ void	ht_destroy(t_kv *ht[TABLE_SIZE])
 	int		i;
 	t_kv	*prev;
 	t_kv	*kv;
-	int		j;
 
 	i = -1;
 	while (++i < TABLE_SIZE)
 	{
 		kv = ht[i];
-		j = 0;
 		while (kv)
 		{
 			free(kv->k);
 			free(kv->v.as_str);
 			prev = kv;
 			kv = kv->n;
-			if (j > 0)
-				free(prev);
-			++j;
+			free(prev);
 		}
 	}
 }
