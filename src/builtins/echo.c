af29392e81fe1479f29db8cd8a173987b091ca4e
pajolinho <pgrussin@student.42berlin.de>
2024-05-18 18:11:02 +0200

    finish bulitins echo exit

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,25 +3,44 @@
 
 int	builtin_echo(char **argv, t_fds fds)
 {
-	int		i;
+	int	i;
+	t_bool	flag;
+	int	j;
 
-	// TODO: implement -n when first argument
+	flag = FALSE;	
 	i = 0;
+	j = 0;
+	if (argv[0] != NULL && ft_strcmp(argv[0], "-n") == 0)
+	{
+		flag = TRUE;
+		i++;
+		j++;
+	}
 	while(argv[i] != NULL)
 	{
-		if (i > 0)
-			ft_dprintf(fds.fd_out, " ");
 		if (fds.fd_out != -2)
+		{
+			if (i > j)
+				ft_dprintf(fds.fd_out, " ");
 			ft_dprintf(fds.fd_out, "%s", argv[i++]);
+		}
 		else
+		{
+			if (i > j)
+				ft_printf(" ");
 			ft_printf("%s", argv[i++]);
+		}
 	}
 	if (fds.fd_out != -2)
 	{
-		ft_dprintf(fds.fd_out, "\n");
+		if (flag == FALSE)
+			ft_dprintf(fds.fd_out, "\n");
 		close(fds.fd_out);
 	}
 	else
-		ft_printf("\n");
+	{
+		if (flag == FALSE)
+			ft_printf("\n");
+	}
 	return (0);
 }
--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -1,6 +1,11 @@
 #include "../../include/minishell.h"
+#include <stdlib.h>
 
 int	builtin_exit(char **argv, t_fds fds)
 {
+	/*TODO: all freed?*/
+	(void)gc_free();
+	rl_clear_history();
+	exit(0);
 	return (0);
 }
