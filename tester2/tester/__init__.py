e41c41cb2694fee59e5cb0adf3242992c1092774
cubernetes <tischmid@proton.me>
2024-07-11 20:24:13 +0200

    Delete almost every non-required file

--- tester2/tester/__init__.py
+++
@@ -1,4 +0,0 @@
-from .setup import get_args, parse_test_file, setup_directories, cleanup_directories
-from .utils import print_total, print_test_block_options, print_minishell_not_found
-from .test_logic import run_tests
-from .config import globals, minishell_path
--- valgrind-open-fds.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh -
-
-valgrind -q --log-file=>(grep -v '^==[[:digit:]]\+==\s*$') --track-fds=all ./minishell
